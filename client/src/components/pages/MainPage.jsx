48cc40f7bfe05fdcc37cf4f68e8c5e4d11d00d3f
Sandraaaaa <S84022977@gmail.com>
2025-03-12 18:00:52 +0300

    logo

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './MainPage.css';
 
 export default function MainPage() {
+  const navigate = useNavigate(); 
+
+  const handleRegisterClick = () => {
+    navigate('/reg'); 
+  };
+
+  const handleLoginClick = () => {
+    navigate('/login'); 
+  };
+
   return (
     <div id="wrapper">
       <section id="content">
         <section className="title container flow--lg">
+          <div className="logo-container">
+            <img src="/keb.png" alt="Kebab" className="logo" />
+          </div>
+
           <h1>
             <span className="eyebrow" aria-hidden="true">Добро пожаловать в </span>
             Кебаб-Маркет
@@ -14,8 +29,8 @@ export default function MainPage() {
             Мы предлагаем уникальное решение для тех, кто не хочет, чтобы еда пропадала зря! Если заказчик не принял доставку, вы можете разместить непринятый заказ в нашем приложении, а люди рядом смогут его выкупить со скидкой.
           </p>
           <div className="buttons">
-            <button className="btn-register">Регистрация</button>
-            <button className="btn-login">Вход</button>
+            <button className="btn-register" onClick={handleRegisterClick}>Регистрация</button>
+            <button className="btn-login" onClick={handleLoginClick}>Вход</button>
           </div>
         </section>
 
