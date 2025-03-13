07f25492a4ee11a31bba74fd828402b4cbbd25a5
ahmadieved <Ahmadiev9696@yandex.com>
2025-03-13 11:42:46 +0500

    without conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/pages/MainPage';
 import RegisterPage from './components/pages/RegistrationForm';
