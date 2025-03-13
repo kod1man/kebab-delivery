353faa903336191e84060044315b566d361b967e
Sandraaaaa <S84022977@gmail.com>
2025-03-13 13:22:13 +0300

    rd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import MainPage from './components/pages/MainPage';
 import RegisterPage from './components/pages/RegistrationForm';
 import CourierPage from './components/pages/CourierPage';
 import LoginPage from './components/pages/LoginPage';
+import CustomerPage from './components/pages/CustomerPage'; 
 import axiosInstance, { setAccessToken } from './api/axiosInstance';
 
 function App() {
@@ -23,12 +24,18 @@ function App() {
       });
   }, []);
 
+  const handleLogout = () => {
+    setUser({ status: 'guest', data: null }); 
+    setAccessToken(''); 
+  };
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<MainPage user={user} />} />
+        <Route path="/" element={<MainPage user={user} onLogout={handleLogout} />} />
         <Route path="/reg" element={<RegisterPage setUser={setUser} />} />
-        <Route path="/courier" element={<CourierPage />} />
+        <Route path="/courier" element={<CourierPage user={user} onLogout={handleLogout} />} />
+        <Route path="/customer" element={<CustomerPage user={user} onLogout={handleLogout} />} />
         <Route path="/login" element={<LoginPage setUser={setUser} />} />
       </Routes>
     </div>
--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
-import axiosInstance, { setAccessToken } from '../../api/axiosInstance';
+import Navbar from '../ui/NavBar'; 
 
 export default function LoginPage({ setUser }) {
   const navigate = useNavigate();
+
   const handleLogin = (e) => {
     e.preventDefault();
     const formData = Object.fromEntries(new FormData(e.target));
@@ -19,11 +20,11 @@ export default function LoginPage({ setUser }) {
 
   return (
     <div className="login-container">
+      <Navbar user={{ status: 'guest', data: null }} onLogout={() => {}} /> 
       <form onSubmit={handleLogin} className="login-form">
         <h2>Вход в аккаунт</h2>
         <div className="form-group">
           <label htmlFor="email">Почта:</label>
-
           <input type="email" id="email" name="email" required />
         </div>
 
